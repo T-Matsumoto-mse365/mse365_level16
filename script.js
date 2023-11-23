6d5e5b222ed70e53c562b6ea18f8ce6e36a1e6cc
T-Matsumoto-mse365 <ma2-ochop@outlook.jp>
2023-11-24 06:40:16 +0900

    課題 の提出
    script.js　課題の解答を追記/NEXT.mdを新規作成

--- script.js
+++ script.js
@@ -11,6 +11,13 @@
  */
 
 // ここに関数を定義しましょう
+const over100 = function (number) {
+    if (number >= 100) {
+        return true ;
+    } else {
+        return false ;
+    }
+}
 
 
 // ここからはテストなので変更を加えないでください
